aabe0c02e2e0c398e3c5a3fb798141d5026e06fa
Cody Robbins <cody@codyrobbins.com>
2013-03-22 23:02:02 -0400

    Initial Commit

---
+++ inconsistent-cocoa-bindings/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  inconsistent-cocoa-bindings
+//
+//  Created by Cody on 22/03/2013.
+//  Copyright (c) 2013 Cody Robbins. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ inconsistent-cocoa-bindings/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  inconsistent-cocoa-bindings
+//
+//  Created by Cody on 22/03/2013.
+//  Copyright (c) 2013 Cody Robbins. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+  return NSApplicationMain(argc, (const char **)argv);
+}
